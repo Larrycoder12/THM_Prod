1607ab23f0ad386d47435fe7294ef027f63e2126
coolguycolin4409-coder <coolguycolin4409@gmail.com>
2026-03-01 12:45:41 -0800

    PAUSE MENU FINISHED ITS BEEN FOUR HOURS KHGDFKUYFGYU

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,20 +1,28 @@
 extends Control
-func resume():
-	get_tree().paused=false
-	$AnimationPlayer.play_backwards("blur")
+
+func _ready():
+	hide()
+	get_tree().paused = false
+
+func _process(delta):
+	if Input.is_action_just_pressed("esc"):
+		if get_tree().paused:
+			resume()
+		else:
+			pause()
+
 func pause():
-	get_tree().paused=true
+	get_tree().paused = true
+	show()
+	$AnimationPlayer.play("blur")
+
+func resume():
+	get_tree().paused = false
 	$AnimationPlayer.play_backwards("blur")
-	
-func testEsc():
-	if Input.is_action_just_pressed("esc") and get_tree().paused:
-		pause()
-	elif Input.is_action_just_pressed("esc") and get_tree().paused:
-		resume()
+	hide()
 
 func _on_resume_pressed() -> void:
-	resume() # Replace with function body.
-
+	resume()
 
 func _on_quit_pressed() -> void:
-	get_tree().quit() # Replace with function body.
+	get_tree().quit()

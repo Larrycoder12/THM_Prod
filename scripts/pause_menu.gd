af9b23368f14c32c74c4159016fe86b7e0f08941
coolguycolin4409-coder <coolguycolin4409@gmail.com>
2026-03-01 11:31:45 -0800

    push or git mad

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,9 +1,10 @@
 extends Control
 func resume():
 	get_tree().paused=false
-	
+	$AnimationPlayer.play_backwards("blur")
 func pause():
 	get_tree().paused=true
+	$AnimationPlayer.play_backwards("blur")
 	
 func testEsc():
 	if Input.is_action_just_pressed("esc") and get_tree().paused:

6212e1ac7734857b2aea6b36f6ec52758ee7db7d
worldclassdingus <ryanpxp5@gmail.com>
2026-03-01 09:28:45 -0800

    Changed player movement to accelration and added a jetpack

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 
-const ACCELERATION = 5
-const MAX_VELOCITY = 400
-const JUMP_VELOCITY = -200.0
+const ACCELERATION = 200
+const MAX_VELOCITY = 200
+const BOUNCE = 40
+const JETPACK_VELOCITY = -80.0
 
 
 func _physics_process(delta: float) -> void:
@@ -12,17 +13,17 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_pressed("up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_pressed("up"):
+		velocity.y = JETPACK_VELOCITY
 
 	if Input.is_action_pressed("left"):
 		if velocity.x > -1 * MAX_VELOCITY:
-			velocity.x -= ACCELERATION
+			velocity.x -= ACCELERATION * delta
 	elif Input.is_action_pressed("right"):
 		if velocity.x < MAX_VELOCITY:
-			velocity.x += ACCELERATION
+			velocity.x += ACCELERATION * delta
 	else:
-		velocity.x -= sign(velocity.x) * ACCELERATION
+		velocity.x -= sign(velocity.x) * ACCELERATION * delta
 	
 	if velocity.x < 0:
 		$Sprite2D.flip_h = true
@@ -30,3 +31,20 @@ func _physics_process(delta: float) -> void:
 		$Sprite2D.flip_h = false
 
 	move_and_slide()
+	
+	for i in range(get_slide_collision_count()):
+		var col := get_slide_collision(i)
+	
+		var n := col.get_normal()
+		if abs(n.x) > abs(n.y):
+			# side hit
+			if n.x > 0: # hit left
+				velocity.x += BOUNCE
+			else: # hit right
+				velocity.x -= BOUNCE
+		else:
+			# vertical hit
+			if n.y > 0: # hit ceiling
+				velocity.y += BOUNCE
+			else: # hit floor
+				velocity.y = -1 * BOUNCE * 4

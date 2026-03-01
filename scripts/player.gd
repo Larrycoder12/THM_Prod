b1372714b906d994a00c2837ee19c5d44de97cfc
Finnley Bronson <finnleybronson@gmail.com>
2026-03-01 09:49:31 -0800

    Added mining and cleaned up movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,19 @@
 extends CharacterBody2D
 
-
 const ACCELERATION = 200
 const MAX_VELOCITY = 200
 const BOUNCE = 40
 const JETPACK_VELOCITY = -80.0
 const SPEED = 100.0
 const JUMP_VELOCITY = -150.0
-
+const BREAK_THRESHOLD = 120.0 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	var pre_collision_velocity = velocity
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_pressed("up"):
 		velocity.y = JETPACK_VELOCITY
 
@@ -25,7 +24,7 @@ func _physics_process(delta: float) -> void:
 		if velocity.x < MAX_VELOCITY:
 			velocity.x += ACCELERATION * delta
 	else:
-		velocity.x -= sign(velocity.x) * ACCELERATION * delta
+		velocity.x = move_toward(velocity.x, 0, ACCELERATION * delta)
 	
 	if velocity.x < 0:
 		$Sprite2D.flip_h = true
@@ -36,17 +35,29 @@ func _physics_process(delta: float) -> void:
 	
 	for i in range(get_slide_collision_count()):
 		var col := get_slide_collision(i)
-	
 		var n := col.get_normal()
+		var collider = col.get_collider()
+		
+		if collider is TileMapLayer:
+			var impact_force = pre_collision_velocity.dot(n)
+			
+			if abs(impact_force) > BREAK_THRESHOLD:
+				var hit_point = col.get_position()
+				var map_pos = collider.local_to_map(collider.to_local(hit_point - n * 4))
+				
+				if collider.get_cell_source_id(map_pos) != -1:
+					break_block(collider, map_pos)
+		
 		if abs(n.x) > abs(n.y):
-			# side hit
-			if n.x > 0: # hit left
+			if n.x > 0: 
 				velocity.x += BOUNCE
-			else: # hit right
+			else: 
 				velocity.x -= BOUNCE
 		else:
-			# vertical hit
-			if n.y > 0: # hit ceiling
+			if n.y > 0: 
 				velocity.y += BOUNCE
-			else: # hit floor
+			else: 
 				velocity.y = -1 * BOUNCE * 4
+
+func break_block(layer: TileMapLayer, map_pos: Vector2i):
+	layer.erase_cell(map_pos)
